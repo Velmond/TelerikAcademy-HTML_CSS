e8712a63f9b7fffee8cfbcb7b92c6b6e87398d56
Mitko Nikolov <mitko.nikoloff@gmail.com>
2014-07-05 16:21:47 +0300

    Added HTML and CSS homework

---
+++ CSS/Layout/05.LaptopCatalogue/SliderValue.js
@@ -0,0 +1,12 @@
+﻿function printValue(sliderID, textboxID) {
+    var x = document.getElementById(textboxID);
+    var y = document.getElementById(sliderID);
+    x.innerText = y.value;
+}
+
+window.onload = function () {
+    printValue('minPriceSlider', 'minPrice');
+    printValue('maxPriceSlider', 'maxPrice');
+    printValue('minRAMSlider', 'minRAM');
+    printValue('maxRAMSlider', 'maxRAM');
+}
